19a8ba4b27379ee33427a992f3d574ff73d86b7e
DmitryLutskovich <lutskovich.dima.99@mail.ru>
2018-09-25 23:52:31 +0300

    Money-Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	let result = {};
+	if (currency > 10000)
+	{
+		result.error = "You are rich, my friend! We don't have so much coins for exchange";
+		return result;
+	}
+	let numbercoins = {"H": 50, "Q": 25, "D": 10, "N": 5, "P": 1};
+	for(let coin in numbercoins) {
+		let count=0;
+		while (currency - numbercoins[coin] >= 0) {
+			count++;
+			currency -= numbercoins[coin];
+		}
+		if (count > 0) {
+			result[coin] = count;
+		}
+	}
+	return result;
 }
